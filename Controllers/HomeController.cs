16ac87562716a4ce7f0a05cbbd355b7361c413c7
sena-efkk <senasultankar@gmail.com>
2026-01-28 03:26:48 +0300

    add viewbag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,18 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using asp.netcoreIdentityApp.Web.Models;
+using asp.netcoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace asp.netcoreIdentityApp.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<AppUser> _Usermanager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(UserManager<AppUser> u)
     {
-        _logger = logger;
+        _Usermanager=u;
     }
 
     public IActionResult Index()
@@ -24,6 +26,25 @@ public class HomeController : Controller
     }
     public IActionResult SingUp()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SingUp(SignUpViewModel request)
+    {
+
+        var identityResult =await _Usermanager.CreateAsync(new() {
+                UserName=request.UserName ,PhoneNumber=request.Phone,Email=request.Email},request.PasswordConfirm);
+        if (identityResult.Succeeded)
+        {
+            ViewBag.SuccessMessage="Üyelik işlemi başarıyla tmamlandı :)";
+            return View();
+        }
+
+        foreach(IdentityError item in identityResult.Errors)
+        {
+            ModelState.AddModelError(string.Empty,item.Description);
+        }
         return View();
     }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
