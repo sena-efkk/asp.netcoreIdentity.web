28b3ecf730201cb6b61af90401f9bf8120d38194
sena-efkk <senasultankar@gmail.com>
2026-02-06 09:58:21 +0300

    add returnURL

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,9 +1,11 @@
 using asp.netcoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace asp.netcoreIdentityApp.Web.Controllers
 {  
+    [Authorize]
     public class MemberController :Controller
     {
         private readonly SignInManager<AppUser> _signinM;
@@ -12,6 +14,10 @@ namespace asp.netcoreIdentityApp.Web.Controllers
             this._signinM=sm;
         }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
         public async Task Logout()
         {
             await  _signinM.SignOutAsync();
